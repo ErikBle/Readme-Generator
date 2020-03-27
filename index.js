c7513010e773df0b9506856c1aad98b976635231
Yfopla <ekblecker@gmail.com>
2020-03-27 11:57:19 -0400

    Added: Starting questions

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-const questions = [
+const fs = require("fs");
+const axios = require("axios");
+const inquirer = require("inquirer");
+const util = require("util");
 
+
+const questions = [{
+    message: "Enter your GitHub username",
+    name: "username"
+},{
+    message: "Create the title of your badge",
+    name: "badgeLabel"
+},{ 
+    type: 'list',
+    message: "Choose the color of your badge",
+    name: "badgeColor",
+    choices: ["green", "yellow", "orange", "red", "blue"]
+},
 ];
 
-function writeToFile(fileName, data) {
-}
 
-function init() {
-
-}
-
-init();
