0f07ede7752dad60181c42f70c35961444801e38
Yfopla <ekblecker@gmail.com>
2020-03-27 15:15:00 -0400

    Added: starting functions defined

--- index.js
+++ index.js
@@ -2,20 +2,39 @@ const fs = require("fs");
 const axios = require("axios");
 const inquirer = require("inquirer");
 const util = require("util");
+const writeFileAsync = util.promisify(fs.writeFile);
 
-
-const questions = [{
+function promptUser() {
+    return inquirer.prompt ([
+{
     message: "Enter your GitHub username",
     name: "username"
 },{
     message: "Create the title of your badge",
-    name: "badgeLabel"
+    name: "badgeTitle"
 },{ 
     type: 'list',
     message: "Choose the color of your badge",
     name: "badgeColor",
     choices: ["green", "yellow", "orange", "red", "blue"]
 },
-];
+]);
+}
 
-
+async function init() {
+    console.log("hi")
+    try {
+      const answers = await promptUser();
+  
+      const html = generateHTML(answers);
+  
+      await writeFileAsync(`${answers.name}.html`, html);
+  
+      console.log("Successfully wrote to index.html");
+    } catch(err) {
+      console.log(err);
+    }
+  }
+  
+  init();
+  
